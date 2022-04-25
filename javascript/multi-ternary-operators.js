9c1051587257a3d3bba384a9b3db9cd80ffc2624
angelptli <ali95@mail.ccsf.edu>
2022-04-24 21:18:18 -0700

    Use multiple conditional (ternary) operators to check if a number is positive, negative, or equal to zero in one return statement

---
+++ javascript/multi-ternary-operators.js
@@ -0,0 +1,14 @@
+/**
+ * Check whether a number is positive, negative, or equal to zero
+ * @param {Number} num The number being tested
+ * @returns A string indicating positive, negative, or zero
+ */
+function checkSign(num) {
+  return (num > 0) ? "positive"
+    : (num < 0) ? "negative"
+    : "zero";
+}
+
+checkSign(10)  // should return the string positive
+checkSign(-12) // should return the string negative
+checkSign(0)   // should return the string zero

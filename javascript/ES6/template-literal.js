abe49f953c47eb8d6ec4c3fa500f5c79f3b685dd
angelptli <ali95@mail.ccsf.edu>
2022-05-01 14:55:04 -0700

    Use template literals to create multi-line strings and include variables values

---
+++ javascript/ES6/template-literal.js
@@ -0,0 +1,23 @@
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+
+/**
+ * Add to an array the result object's failure property included in
+ * list element (li) string.
+ * @param {*Object} arr The object's property
+ * @returns An array of failure items.
+ */
+function makeList(arr) {
+  const failureItems = [];
+
+  // Use a template literal to create each string in the array
+  for (let i = 0; i < 3; i++)
+    failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
+
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);

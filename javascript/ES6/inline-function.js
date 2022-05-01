c637c4b05a9ba01296ef1a6b2027c22966678ba1
angelptli <ali95@mail.ccsf.edu>
2022-05-01 12:00:39 -0700

    Write inline functions that accept parameters and set default parameters

--- javascript/ES6/inline-function.js
+++ javascript/ES6/inline-function.js
@@ -8,3 +8,14 @@ const magic1 = function() {
 const magic2 = () => {
   return new Date();
 };
+
+// Write inline functions that accept parameters
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+console.log(myConcat([1, 2], [3, 4, 5])); // display [1, 2, 3, 4, 5]
+
+const num = arg => arg * 2;
+console.log(num(4)); // display 8
+
+// Set default parameter to so that it will add 1 to number if value is not
+// specified.
+const increment = (number, value = 1) => number + value;

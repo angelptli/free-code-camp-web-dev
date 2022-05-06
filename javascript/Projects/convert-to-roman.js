785fdf6f30952379476c3fbf76563f99a8485c63
angelptli <ali95@mail.ccsf.edu>
2022-05-06 13:35:37 -0700

    Create a number to roman numeral converter program that supports converting numbers 1 to 3999. This is 2 of 5 js projects completed.

---
+++ javascript/Projects/convert-to-roman.js
@@ -0,0 +1,95 @@
+/**
+ * Convert the given number num into a roman numeral. This function
+ * supports converting numbers in between 1 and 3999 into roman numerals.
+ * @param {*Number} num A number to be converted into a roman number.
+ * @returns The roman numeral string of the given num.
+ */
+function convertToRoman(num) {
+  let commonNums = [
+    {'I': '1'}, {'II': '2'}, {'III': '3'}, {'IV': '4'}, {'V': '5'},
+    {'VI': '6'}, {'VII': '7'}, {'VIII': '8'}, {'IX': '9'}, {'X': '10'},
+    {'XX': '20'}, {'XXX': '30'}, {'XL': '40'}, {'L': '50'}, {'LX': '60'},
+    {'LXX': '70'}, {'LXXX': '80'}, {'XC': '90'}, {'C': '100'},
+    {'CC': '200'}, {'CCC': '300'}, {'CD': '400'}, {'D': '500'},
+    {'DC': '600'}, {'DCC': '700'}, {'DCCC': '800'}, {'CM': '900'},
+    {'M': '1000'}, {'MM': '2000'}, {'MMM': '3000'}
+  ];
+
+  // Convert num to a string
+  let strNum = num.toString();
+
+  // If strNum is found in commonNums, return its key
+  let romanNum = commonNums.filter(prop =>
+    strNum == Object.values(prop)
+  );
+  if (romanNum.length > 0) {
+    return Object.keys(romanNum[0])[0];
+  }
+
+  // Reset strNum as a string version of num
+  strNum = num.toString();
+
+  // Create an array version of strNum
+  let arrNum = strNum.split("");
+
+  // If num > 10, pad the numbers greater than 10. Increase by
+  // one zero pad with each new iteration.
+  if (num > 10) {
+    let zero = "0";
+
+    for (let n = arrNum.length - 2; n >= 0; n--) {
+      if (n != arrNum.length - 1) {
+        arrNum[n] = arrNum[n] + zero;
+        zero += "0";
+      }
+    }  
+  }
+
+  let value = "";
+  let index = 0;
+
+  // Replace arrNum elements with their corresponding roman numerals
+  commonNums.filter(n => {
+    value = Object.values(n);
+    index = arrNum.indexOf(value[0]);
+
+    if(arrNum[index]){ 
+      arrNum[index] = Object.keys(n)[0];
+    }
+  });
+
+  // Join array of roman numerals and remove pure 0 elements
+  // that originally 0's found in between the original num.
+  // E.g., 501, 1004, 1006, 2014.
+  arrNum = arrNum.join("").replace(/0/g, "");
+
+  return arrNum;
+}
+
+// Tests
+console.assert(convertToRoman(2), "should return the string II");
+console.assert(convertToRoman(3), "should return the string III");
+console.assert(convertToRoman(4), "should return the string IV");
+console.assert(convertToRoman(5), "should return the string V");
+console.assert(convertToRoman(9), "should return the string IX");
+console.assert(convertToRoman(12), "should return the string XII");
+console.assert(convertToRoman(16), "should return the string XVI");
+console.assert(convertToRoman(29), "should return the string XXIX");
+console.assert(convertToRoman(44), "should return the string XLIV");
+console.assert(convertToRoman(45), "should return the string XLV");
+console.assert(convertToRoman(68), "should return the string LXVIII");
+console.assert(convertToRoman(83), "should return the string LXXXIII");
+console.assert(convertToRoman(97), "should return the string XCVII");
+console.assert(convertToRoman(99), "should return the string XCIX");
+console.assert(convertToRoman(400), "should return the string CD");
+console.assert(convertToRoman(500), "should return the string D");
+console.assert(convertToRoman(501), "should return the string DI");
+console.assert(convertToRoman(649), "should return the string DCXLIX");
+console.assert(convertToRoman(798), "should return the string DCCXCVIII");
+console.assert(convertToRoman(891), "should return the string DCCCXCI");
+console.assert(convertToRoman(1000), "should return the string M");
+console.assert(convertToRoman(1004), "should return the string MIV");
+console.assert(convertToRoman(1006), "should return the string MVI");
+console.assert(convertToRoman(1023), "should return the string MXXIII");
+console.assert(convertToRoman(2014), "should return the string MMXIV");
+console.assert(convertToRoman(3999), "should return the string MMMCMXCIX");

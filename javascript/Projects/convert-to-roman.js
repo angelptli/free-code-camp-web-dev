938be6d7a5c194701a3d42c4439ef79c0b839139
angelptli <ali95@mail.ccsf.edu>
2022-05-06 13:38:56 -0700

    Modify tests in convert-to-roman.js project to display test output

--- javascript/Projects/convert-to-roman.js
+++ javascript/Projects/convert-to-roman.js
@@ -67,29 +67,107 @@ function convertToRoman(num) {
 }
 
 // Tests
+// should return the string II
+console.log(convertToRoman(2));
 console.assert(convertToRoman(2), "should return the string II");
+
+// should return the string III
+console.log(convertToRoman(3));
 console.assert(convertToRoman(3), "should return the string III");
+
+// should return the string IV
+console.log(convertToRoman(4));
 console.assert(convertToRoman(4), "should return the string IV");
+
+// should return the string V
+console.log(convertToRoman(5));
 console.assert(convertToRoman(5), "should return the string V");
+
+// should return the string IX
+console.log(convertToRoman(9));
 console.assert(convertToRoman(9), "should return the string IX");
+
+// should return the string XII
+console.log(convertToRoman(12));
 console.assert(convertToRoman(12), "should return the string XII");
+
+// should return the string XVI
+console.log(convertToRoman(16));
 console.assert(convertToRoman(16), "should return the string XVI");
+
+// should return the string XXIX
+console.log(convertToRoman(29));
 console.assert(convertToRoman(29), "should return the string XXIX");
+
+// should return the string XLIV
+console.log(convertToRoman(44));
 console.assert(convertToRoman(44), "should return the string XLIV");
+
+// should return the string XLV
+console.log(convertToRoman(45));
 console.assert(convertToRoman(45), "should return the string XLV");
+
+// should return the string LXVIII
+console.log(convertToRoman(68));
 console.assert(convertToRoman(68), "should return the string LXVIII");
+
+// should return the string LXXXIII
+console.log(convertToRoman(83));
 console.assert(convertToRoman(83), "should return the string LXXXIII");
+
+// should return the string XCVII
+console.log(convertToRoman(97));
 console.assert(convertToRoman(97), "should return the string XCVII");
+
+// should return the string XCIX
+console.log(convertToRoman(99));
 console.assert(convertToRoman(99), "should return the string XCIX");
+
+// should return the string CD
+console.log(convertToRoman(400));
 console.assert(convertToRoman(400), "should return the string CD");
+
+// should return the string D
+console.log(convertToRoman(500));
 console.assert(convertToRoman(500), "should return the string D");
+
+// should return the string DI
+console.log(convertToRoman(501));
 console.assert(convertToRoman(501), "should return the string DI");
+
+// should return the string DCXLIX
+console.log(convertToRoman(649));
 console.assert(convertToRoman(649), "should return the string DCXLIX");
+
+// should return the string DCCXCVIII
+console.log(convertToRoman(798));
 console.assert(convertToRoman(798), "should return the string DCCXCVIII");
+
+// should return the string DCCCXCI
+console.log(convertToRoman(891));
 console.assert(convertToRoman(891), "should return the string DCCCXCI");
+
+// should return the string M
+console.log(convertToRoman(1000));
 console.assert(convertToRoman(1000), "should return the string M");
+
+// should return the string MIV
+console.log(convertToRoman(1004));
 console.assert(convertToRoman(1004), "should return the string MIV");
+
+// should return the string MVI
+console.log(convertToRoman(1006));
 console.assert(convertToRoman(1006), "should return the string MVI");
+
+// should return the string MXXIII
+console.log(convertToRoman(1023));
 console.assert(convertToRoman(1023), "should return the string MXXIII");
+
+// should return the string MMXIV
+console.log(convertToRoman(2014));
 console.assert(convertToRoman(2014), "should return the string MMXIV");
+
+// should return the string MMMCMXCIX
+console.log(convertToRoman(3999));
 console.assert(convertToRoman(3999), "should return the string MMMCMXCIX");
+
